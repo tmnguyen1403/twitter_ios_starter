13ddf77051fc9fd6d41ebabb758eeee29f8cf108
Minh Nguyen <tmnguyen1403@gmail.com>
2020-10-06 00:32:41 -0500

    10/6/2020 Implement refreshControl for HomeView

--- Twitter/Home/HomeTableViewController.swift
+++ Twitter/Home/HomeTableViewController.swift
@@ -12,13 +12,19 @@ class HomeTableViewController: UITableViewController {
   
     var tweetArray = [NSDictionary]();
     var numberOfTweet: Int!;
+    
+    let myRefreshControl = UIRefreshControl();
   
     override func viewDidLoad() {
         super.viewDidLoad()
-      loadTweet();
+        loadTweet();
+        
+      myRefreshControl.addTarget(self, action: #selector(loadTweet), for: .valueChanged);
+      tableView.refreshControl = myRefreshControl;
     }
+  
     
-  func loadTweet() {
+  @objc func loadTweet() {
     let url = "https://api.twitter.com/1.1/statuses/home_timeline.json";
     let myParams = ["count": 10];
     
@@ -30,6 +36,7 @@ class HomeTableViewController: UITableViewController {
         self.tweetArray.append(tweet);
       }
       self.tableView.reloadData();
+      self.myRefreshControl.endRefreshing();
       
     }, failure: { (Error) in
       print("Could not retrieve tweets!");

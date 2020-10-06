065a43d6fc269387985676569ae983a6de0c6996
Minh Nguyen <tmnguyen1403@gmail.com>
2020-10-05 23:50:26 -0500

    10/05/2020 Add custom cell for Home View

--- Twitter/Home/HomeTableViewController.swift
+++ Twitter/Home/HomeTableViewController.swift
@@ -21,16 +21,21 @@ class HomeTableViewController: UITableViewController {
       self.dismiss(animated: true, completion: nil);
     }
   
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "tweetCell", for: indexPath);
+    return cell;
+  }
+  
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 5
     }
 
     /*
